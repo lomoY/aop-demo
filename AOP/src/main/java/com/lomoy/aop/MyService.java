f53f92895bac3d56bd5e577a6a93325af62e78a0
Lomo <lomo.yeung@gmail.com>
2024-11-26 14:55:30 -0500

    hellow world

---
+++ AOP/src/main/java/com/lomoy/aop/MyService.java
@@ -0,0 +1,50 @@
+package com.lomoy.aop;
+
+import org.springframework.aop.framework.AopContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+
+//    @MyLog(myData="VC test")
+    public void publicMethodA(){
+        /**
+         *
+         * AopContext.currentProxy
+         * 可以创建一个方法，然后用This
+         *
+         */
+//        System.out.println(Thread.currentThread().getName() + ": This is methodA");
+        MyService myService = AopContext.currentProxy() !=null ? (MyService)AopContext.currentProxy():this;
+        if (AopContext.currentProxy()==null) {
+            System.out.println("proxy is null");
+        }
+        myService.publicMethodB();
+//        myService.privateMethodB();
+
+        String staticString =  MyStaticClass.staticGetString();
+        System.out.println("StaticMethod called: " + staticString);
+//        this.methodB();
+    }
+
+
+    @MyLog(myData="VC test")
+    public void publicMethodB() {
+        System.out.println(Thread.currentThread().getName() + ": This is methodB");
+        MyService myService = AopContext.currentProxy() !=null ? (MyService)AopContext.currentProxy():this;
+        myService.publicMethodC();
+    }
+
+    @MyLog(myData="VC test")
+    public void publicMethodC() {
+        return;
+    }
+
+
+    @MyLog(myData="VC test")
+    private void privateMethodB() {
+        System.out.println(Thread.currentThread().getName() + ": This is methodB");
+    }
+
+}
---
+++ AOP/src/main/java/com/lomoy/aop/MyStaticClass.java
@@ -0,0 +1,13 @@
+package com.lomoy.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyStaticClass {
+
+
+    @MyLog(myData="VC test")
+    public static String staticGetString(){
+        return "This is a string";
+    }
+}
